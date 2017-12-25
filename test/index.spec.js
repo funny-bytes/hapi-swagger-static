b0266659901f2931277b513ed3a7d6cb30cc1359
Frank Thelen <frank.thelen@telefonica.com>
2017-12-25 22:25:03 +0100

    unit test

--- test/index.spec.js
+++ test/index.spec.js
@@ -16,7 +16,7 @@ global.sinon = sinon;
 global.expect = chai.expect;
 global.should = chai.should();
 
-async function setup() {
+async function setup({ pluginOptions = {} }) {
   const server = new Hapi.Server({
     port: 9001,
   });
@@ -31,8 +31,15 @@ async function setup() {
   await server.route(route);
   await server.register({ plugin: Inert });
   await server.register({ plugin: Vision });
-  await server.register({ plugin: HapiSwagger, options: {} });
-  await server.register({ plugin: HapiSwaggerStatic, options: {} });
+  await server.register({
+    plugin: HapiSwagger,
+    options: {
+      info: {
+        title: 'API Documentation 4711',
+      },
+    },
+  });
+  await server.register({ plugin: HapiSwaggerStatic, options: pluginOptions });
   await server.start();
   return server;
 }
@@ -41,7 +48,7 @@ describe('hapi-swagger-static with default options', async () => {
   let server;
 
   beforeEach(async () => {
-    server = await setup();
+    server = await setup({});
   });
 
   afterEach(async () => {
@@ -58,9 +65,39 @@ describe('hapi-swagger-static with default options', async () => {
         expect(statusCode).to.be.equal(200);
         expect(headers).to.have.property('content-type');
         expect(headers['content-type']).to.be.equal('text/html; charset=utf-8');
+        expect(headers['cache-control']).to.be.equal('max-age=3600, must-revalidate, public');
         expect(payload).to.contain('<html>');
         expect(payload).to.contain('<!DOCTYPE html>');
-        expect(payload).to.contain('<title>API documentation</title>');
+        expect(payload).to.contain('<title>API Documentation 4711</title>');
+        expect(payload).to.contain('/test4711');
+      }));
+});
+
+describe('hapi-swagger-static with `path` option', async () => {
+  let server;
+
+  beforeEach(async () => {
+    server = await setup({ pluginOptions: { path: '/printable.html' } });
+  });
+
+  afterEach(async () => {
+    await server.stop();
+  });
+
+  it('should provide route `printable.html`', () =>
+    server
+      .inject({
+        url: '/printable.html',
+      })
+      .should.be.fulfilled.then((response) => {
+        const { statusCode, headers, payload } = response;
+        expect(statusCode).to.be.equal(200);
+        expect(headers).to.have.property('content-type');
+        expect(headers['content-type']).to.be.equal('text/html; charset=utf-8');
+        expect(headers['cache-control']).to.be.equal('max-age=3600, must-revalidate, public');
+        expect(payload).to.contain('<html>');
+        expect(payload).to.contain('<!DOCTYPE html>');
+        expect(payload).to.contain('<title>API Documentation 4711</title>');
         expect(payload).to.contain('/test4711');
       }));
 });
