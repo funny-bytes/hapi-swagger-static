3a3bf0555a294d68be7e91c7ab28764308a0beae
Frank Thelen <frank.thelen@telefonica.com>
2018-05-03 16:58:58 +0200

    update deps

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const Promise = require('bluebird');
 const SwaggerParser = require('swagger-parser');
 const openapi2html = require('openapi2html');
 const util = require('util');
@@ -22,33 +21,38 @@ const register = async (server, {
     method: 'GET',
     path: route,
     options: auth === undefined ? { cache } : { cache, auth },
-    handler: async (request, h) => Promise.try(async () => {
-      const parser = new SwaggerParser();
-      const api = await parser.parse(`${server.info.uri}${swaggerEndpoint}`, {
-        resolve: {
-          file: false, // don't resolve local file references
-          http: {
-            timeout: 2000,
-            withCredentials: true, // include auth credentials when resolving HTTP references
-            headers: {
-              authorization: request.headers.authorization, // forward
-              ...additionalHeaders,
+    handler: async (request, h) => {
+      try {
+        const parser = new SwaggerParser();
+        const api = await parser.parse(`${server.info.uri}${swaggerEndpoint}`, {
+          resolve: {
+            file: false, // don't resolve local file references
+            http: {
+              timeout: 2000,
+              withCredentials: true, // include auth credentials when resolving HTTP references
+              headers: {
+                authorization: request.headers.authorization, // forward
+                ...additionalHeaders,
+              },
             },
           },
-        },
-      });
-      const html = openapi2html(api, o2hOptions);
-      // the preferred way is to render the page via `view`
-      if (h.view && template) {
-        return h.view(template, { api: { ...api, html } }, viewOptions);
+        });
+        const html = openapi2html(api, o2hOptions);
+        // the preferred way is to render the page via `view`
+        if (h.view && template) {
+          return h.view(template, { api: { ...api, html } }, viewOptions);
+        }
+        // as a fallback, use own template
+        const frame = await readFile(path.join(__dirname, 'frame.html'), 'UTF-8');
+        const page = frame
+          .replace(/\{\{content\}\}/, html)
+          .replace(/\{\{title\}\}/, api.info.title);
+        return h.response(page).type('text/html').charset('utf-8');
+      } catch (error) {
+        request.log(['error'], error);
+        throw error; // -> http 500
       }
-      // as a fallback, use own template
-      const frame = await readFile(path.join(__dirname, 'frame.html'), 'UTF-8');
-      const page = frame
-        .replace(/\{\{content\}\}/, html)
-        .replace(/\{\{title\}\}/, api.info.title);
-      return h.response(page).type('text/html').charset('utf-8');
-    }),
+    },
   });
 };
 
